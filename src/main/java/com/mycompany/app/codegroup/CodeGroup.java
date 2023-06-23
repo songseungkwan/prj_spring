2bada8109ebe7ec1832535f8ff683c45126855ca
songseungkwan <ssk3553@Naver.com>
2023-06-23 17:24:38 +0900

    0623 주말동안 암기하자

---
+++ src/main/java/com/mycompany/app/codegroup/CodeGroup.java
@@ -0,0 +1,30 @@
+package com.mycompany.app.codegroup;
+
+public class CodeGroup {
+
+	// DTO 작성법. (컬럼명과 동일해야한다.)
+	
+	// db 컬럼명 첫글자는 소문자. 
+	// seq는 편의상 디비에는 int지만 자바에서는 String으로 쓴다.
+	// 주로 테이블의 컬럼명과 일치
+	
+	private String cgSeq;					
+	private String cgName;
+// -----
+	public String getCgSeq() {
+		return cgSeq;
+	}
+	public void setCgSeq(String cgSeq) {
+		this.cgSeq = cgSeq;
+	}
+	public String getCgName() {
+		return cgName;
+	}
+	public void setCgName(String cgName) {
+		this.cgName = cgName;
+	}
+	
+	
+	
+	
+}
